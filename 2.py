ccd48f192b9ad9d93cbc1f7fec624139bc560c45
kenranunderscore <johb.maier@gmail.com>
2017-09-10 00:31:13 +0200

    Add solution to problem 2 (python)

---
+++ 2.py
@@ -0,0 +1,16 @@
+def fibonacci(n):
+    if n == 1 or n == 2:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+
+n = 1
+sum = 0
+while True:
+    print(fib)
+    if fib <= 4000000:
+        if fib % 2 == 0:
+            sum += fib
+        n += 1
+    else:
+        break;
+print(sum)
