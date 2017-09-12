9bc596efa701742976f8394971654cfa339674de
kenranunderscore <johb.maier@gmail.com>
2017-09-12 19:56:49 +0200

    Solve problem 14 (python)

---
+++ 14.py
@@ -0,0 +1,19 @@
+def collatz(n):
+    s = []
+    while n != 1:
+        if n % 2 == 0:
+            n = int(n/2)
+        else:
+            n = 3*n + 1
+        s.append(n)
+    return s + [1]
+
+max = 0
+n = 1
+for i in range(1, 1000000):
+    length = len(collatz(i))
+    if length > max:
+        n = i
+        max = length
+print(n)
+print(length)
