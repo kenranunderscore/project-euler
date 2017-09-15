ac8b1cf4c1d6559b99bec090054a708ed9ba7b76
kenranunderscore <johb.maier@gmail.com>
2017-09-15 14:23:56 +0200

    Solve problem 18 (python)
    One solution (other than brute-forcing the problem) is to start at the
    bottom of the triangle, and then adding the maximum of two adjacent
    numbers to their predecessor.

---
+++ 18.py
@@ -0,0 +1,14 @@
+# assumes that the triangle isn't standing on its tip
+def maximum_total(rows):
+    for i in reversed(range(1, len(rows))):
+        for j in range(len(rows[i]) - 1):
+            m = max(rows[i][j], rows[i][j + 1])
+            rows[i - 1][j] += m
+    return rows[0][0]
+
+rows = []
+with open("data18") as f:
+    for line in f:
+        rows.append([int(x) for x in line.split()])
+
+print(maximum_total(rows))
