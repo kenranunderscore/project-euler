9eb35cd01f03cf85245550829da51556cf739f72
kenranunderscore <johb.maier@gmail.com>
2017-09-15 14:40:28 +0200

    Solve problem 67 (python)
    This problem is identical to problem 18. The only difference lies in the
    size of the data attached to it, which makes a brute force attempt
    futile.
    Since we already used a smarter algorithm for problem 20, we can just
    reuse it here.

---
+++ 67/67.py
@@ -0,0 +1,15 @@
+# copy/pasted from problem 18, since only the data is different
+# assumes that the triangle isn't standing on its tip
+def maximum_total(rows):
+    for i in reversed(range(1, len(rows))):
+        for j in range(len(rows[i]) - 1):
+            m = max(rows[i][j], rows[i][j + 1])
+            rows[i - 1][j] += m
+    return rows[0][0]
+
+rows = []
+with open("data") as f:
+    for line in f:
+        rows.append([int(x) for x in line.split()])
+
+print(maximum_total(rows))
