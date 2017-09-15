d8a79a6d220b92661584815fce35dd6d12108380
kenranunderscore <johb.maier@gmail.com>
2017-09-15 12:22:30 +0200

    Improve an algorithm
    The problem 17 algorith was very clunky and contained some code
    repetitions. This is a small attempt at making it more readable and
    understandable by removing those code duplicates.

--- 17.py
+++ 17.py
@@ -26,28 +26,34 @@ words = {
     60:"sixty",
     70:"seventy",
     80:"eighty",
-    90:"ninety"
+    90:"ninety",
+    100:"one hundred",
+    200:"two hundred",
+    300:"three hundred",
+    400:"four hundred",
+    500:"five hundred",
+    600:"six hundred",
+    700:"seven hundred",
+    800:"eight hundred",
+    900:"nine hundred",
 }
 
 def translate(n):
     if n == 1000:
         return "one thousand"
-    hundreds = int(n / 100)
-    m = n - 100 * hundreds
+    m = n % 100
+    hundreds = n - m
+    h = words[hundreds]
     if m == 0 and hundreds > 0:
-        return words[hundreds] + " hundred"
-    elif m <= 20:
-        if hundreds > 0:
-            return words[hundreds] + " hundred and " + words[m]
-        else:
-            return words[m]
+        return h
+    if hundreds > 0:
+        h += " and "
+    if m <= 20:
+        return h + words[m]
     else:
         ones = n % 10
-        tens = n - 100 * hundreds - ones
-        if hundreds > 0:
-            return words[hundreds] + " hundred and " + words[tens] + " " + words[ones]
-        else:
-            return words[tens] + " " + words[ones]
+        tens = n - hundreds - ones
+        return h + words[tens] + " " + words[ones]
 
 translations = map(translate, range(1, 1001))
 lengths = [len(t.replace(" ", "")) for t in translations]
