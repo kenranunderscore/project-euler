38a8f0ea0a6ce15720dafc4d3c5d22faf36f5180
kenranunderscore <johb.maier@gmail.com>
2017-09-17 11:34:56 +0200

    Use // for integer division (2)

--- 23/23.py
+++ 23/23.py
@@ -1,5 +1,5 @@
 def divisor_sum(n):
-    return sum([i for i in range(1, int(n / 2) + 1) if n % i == 0])
+    return sum([i for i in range(1, n // 2 + 1) if n % i == 0])
 
 def is_abundant(n):
     return divisor_sum(n) > n
