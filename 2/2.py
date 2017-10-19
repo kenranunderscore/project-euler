8c6de852be203335e09b4f47a801b45b00be944b
kenranunderscore <johb.maier@gmail.com>
2017-10-19 20:04:45 +0200

    Measure the calculation performance

--- 2/2.py
+++ 2/2.py
@@ -1,16 +1,23 @@
+import time
+
+start_time = time.clock()
+
 def fibonacci(n):
     if n == 1 or n == 2:
         return 1
     return fibonacci(n - 1) + fibonacci(n - 2)
 
 n = 1
-sum = 0
+result = 0
 while True:
     fib = fibonacci(n)
     if fib <= 4000000:
         if fib % 2 == 0:
-            sum += fib
+            result += fib
         n += 1
     else:
         break;
-print(sum)
+
+duration = time.clock() - start_time
+
+print("The result is {} and was found in {} ms.".format(result, duration))
