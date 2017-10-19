7c0118e52f61b913c55a6e05f37b5e5cda034a9d
kenranunderscore <johb.maier@gmail.com>
2017-10-19 19:39:20 +0200

    PEP8ify a bit

--- 1/1.py
+++ 1/1.py
@@ -1,8 +1,9 @@
-def sum_of_multiples_of_3_or_5(max):
-    sum = 0
-    for i in range(0, max):
+def sum_of_multiples_of_3_or_5(threshold):
+    result = 0
+    for i in range(0, threshold):
         if i % 3 == 0 or i % 5 == 0:
-            sum += i
-    return sum
+            result += i
+    return result
+
 
 print(sum_of_multiples_of_3_or_5(1000))
