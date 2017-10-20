1bbd2a1f2edc3945e5aa48f02b9d9c6a4796d548
kenranunderscore <johb.maier@gmail.com>
2017-10-20 08:43:32 +0200

    Time computation and beautify output

--- 3/3.py
+++ 3/3.py
@@ -1,4 +1,8 @@
-def find_largest_prime_factor(n):
+import timeit
+
+start_time = timeit.default_timer()
+
+def largest_prime_factor(n):
     i = 2
     while i * i <= n:
         if n % i == 0:
@@ -7,4 +11,8 @@ def find_largest_prime_factor(n):
             i += 1
     return n
 
-print(find_largest_prime_factor(600851475143))
+duration = timeit.default_timer() - start_time
+
+lpf = largest_prime_factor(600851475143)
+msg = "The largest prime factor of {0} is {1} and was found in {2:.9f} seconds."
+print(msg.format(600851475143, lpf, duration))
