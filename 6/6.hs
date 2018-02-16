5cd53e89f6b4aa148657bfc9e8ec561d40e4a287
kenranunderscore <johb.maier@gmail.com>
2018-02-16 14:02:49 +0100

    Add alternative Haskell solution

--- 6/6.hs
+++ 6/6.hs
@@ -4,3 +4,14 @@ difference n = n^2 * (n+1)^2 `div` 4 - n * (n+1) * (2*n+1) `div` 6
 
 problem6 :: Int
 problem6 = difference 100
+
+-- Solution without using known formulas
+sumOfSquares :: Int -> Int
+sumOfSquares n = sum [x^2 | x <- [1..n]]
+
+squareOfSum :: Int -> Int
+squareOfSum n = s^2
+    where s = sum [1..n]
+
+problem6' :: Int
+problem6' = squareOfSum 100 - sumOfSquares 100
